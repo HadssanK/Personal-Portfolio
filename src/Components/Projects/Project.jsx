cc2cf578d3e4bdd027ee99da44aa04153120e377
AL HABIB TRADE <hassankhan847754@gmail.com>
2025-06-13 14:29:21 -0700

    Updated code with latest changes

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ExternalLink } from "lucide-react";
 import Contentgenrator from '../../assets/Contentgenrator.png'
 import Innercompany from '../../assets/innercompany.png'
@@ -9,6 +9,7 @@ import todo from '../../assets/todo.png'
 import passgen from '../../assets/passgen.png'
 
 export default function Projects() {
+    const [showAll, setShowAll] = useState(false);
   const projects = [
     {
       title: "AI Content Generator",
@@ -67,12 +68,13 @@ export default function Projects() {
       tech: ["React"," Redux Toolkit","Tailwind"],
     }
   ];
-
-  return (
+  const visibleProjects = showAll ? projects : projects.slice(0, 3);
+   return (
     <div className="min-h-screen bg-gray-900 py-10">
       <h1 className="text-4xl text-white font-bold text-center mb-10">My Projects</h1>
+
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 px-5">
-        {projects.map((proj, i) => (
+        {visibleProjects.map((proj, i) => (
           <div
             key={i}
             className="bg-gradient-to-br from-gray-800 via-gray-900 to-black border border-white/10 rounded-xl overflow-hidden shadow-lg transition-all duration-300 w-full max-w-sm mx-auto 
@@ -90,7 +92,6 @@ export default function Projects() {
                 ))}
               </div>
               <div className="flex gap-4">
-              
                 <a href={proj.demo} target="_blank" className="flex items-center gap-2 text-green-400 hover:text-green-300">
                   <ExternalLink size={18} /> Live
                 </a>
@@ -99,6 +100,17 @@ export default function Projects() {
           </div>
         ))}
       </div>
+
+      {projects.length > 3 && (
+        <div className="text-center mt-10">
+          <button
+            onClick={() => setShowAll(!showAll)}
+            className="px-6 py-2 bg-indigo-600 hover:bg-indigo-700 text-white font-medium rounded-lg transition"
+          >
+            {showAll ? "Show Less" : "Show More"}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
